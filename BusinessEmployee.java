c59c41d4d49d1537cd56bfacfeba77b101c6e32f
Oleg Stepanov <oleg.v.stepanov@gmail.com>
2018-10-25 15:20:47 +0200

    Employee, Technical Employee, BusinessEmployee classes added

--- BusinessEmployee.java
+++ BusinessEmployee.java
@@ -1,2 +1,18 @@
-public class BusinessEmployee {
+public class BusinessEmployee extends Employee{
+
+    public BusinessEmployee(String name){
+        super(name,50000);
+    }
+
+    public double getBonusBudget(){
+        return bonusBudget;
+    }
+
+    public void setBonusBudget(double bonusBudget){
+        this.bonusBudget = bonusBudget;
+    }
+
+    public String employeeStatus(){
+        return this.toString() + " with a budget of " + bonusBudget;
+    }
 }
--- TechnicalEmployee.java
+++ TechnicalEmployee.java
@@ -1,2 +1,17 @@
-public class TechnicalEmployee {
+public class TechnicalEmployee extends Employee {
+
+    public int checkin;
+
+    public TechnicalEmployee(String name){
+        super(name, 75000);
+        checkin = 0;
+    }
+
+    public String employeeStatus() {
+        return super.toString() + " has " + checkin + " successful checkins";
+    }
+
+    public void setCheckin(){
+        checkin++;
+    }
 }
